cb840f60025da8871a899a1d3de7546d9e30662a
Islam Ramadan <islamalnms123@gmail.com>
2025-02-25 15:14:17 +0200

    update

--- lib/core/widget/custom_list_view_product.dart
+++ lib/core/widget/custom_list_view_product.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,26 +12,33 @@ class CustomListViewProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var cubit=BlocProvider.of<HomeCubit>(context);
+    var cubit = BlocProvider.of<HomeCubit>(context);
 
     return SliverToBoxAdapter(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.r,vertical: 20),
+        padding: EdgeInsets.symmetric(horizontal: 20.r, vertical: 20),
         child: SizedBox(
           height: 220.h,
-          child: ListView.separated(
-            physics: BouncingScrollPhysics(),
-            scrollDirection: Axis.horizontal,
-            itemCount: cubit.products.length,
-            itemBuilder: (context, index) {
-              return CustomProduct(productModel: cubit.products[index],);
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return SizedBox(
-                width: 10.w,
-              );
-            },
-          ),
+          child: CarouselSlider.builder(
+              itemCount: cubit.products.length,
+              itemBuilder: (context, index, realIndex) {
+                return CustomProduct(
+                  productModel: cubit.products[index],
+                );
+              },
+              options: CarouselOptions(
+                height: 220.h,
+                viewportFraction: 0.5,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeFactor: 0.3,
+                scrollDirection: Axis.horizontal,
+              )),
         ),
       ),
     );
